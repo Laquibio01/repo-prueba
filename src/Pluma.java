2e884bd4359c6a7b66b817f2bc2ce3d865ef9396
Joshgit4905 <Jrc4905@gmail.com>
2024-07-03 09:13:00 -0600

    joshua R - teamoelias
    teamoelias

--- src/Pluma.java
+++ src/Pluma.java
@@ -2,5 +2,6 @@ public class Pluma {
     public static void main(String[] args) {
         General gen = new General();
         gen.datos("Diamante", "Azul", "Plastico", "Pollo", 1);
+        System.out.println("hola");
     }
 }
